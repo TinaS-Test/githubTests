819df2ba9aa77a49b40bff92bf7994398adf16f9
TinaS-Test <silfvertina@gmail.com>
2025-12-23 10:50:05 +0100

    Removed the first messages

--- src/githubTests/HelloWorld.java
+++ src/githubTests/HelloWorld.java
@@ -2,12 +2,11 @@ package githubTests;
 
 public class HelloWorld {
     static void main(String[] args) {
-        System.out.println("Hello Tomas!");
-        System.out.println("Hello class!");
-        System.out.println("Hello world!");
-
         String message = "Hello hello";
         System.out.println("I will no write a message from a string!");
         System.out.println(message);
+
+        String wishes = "Have agreat day";
+        System.out.println(wishes);
     }
 }
