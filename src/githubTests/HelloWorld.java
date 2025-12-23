25cd23bca74951836f469d40b560ad10c786808c
Tina <silfvertina@gmail.com>
2025-12-23 11:08:16 +0100

    Merge pull request #1
    * Removed the first messages
    
    * Removed the first messages
    
    * Removed the first messages
    
    * Removed the first message, and added another

--- src/githubTests/HelloWorld.java
+++ src/githubTests/HelloWorld.java
@@ -1,13 +1,10 @@
 package githubTests;
 
 public class HelloWorld {
-    static void main(String[] args) {
-        System.out.println("Hello Tomas!");
-        System.out.println("Hello class!");
-        System.out.println("Hello world!");
+    static void main() {
+        String wishes = "Have a great day";
+        System.out.println(wishes);
 
-        String message = "Hello hello";
-        System.out.println("I will no write a message from a string!");
-        System.out.println(message);
+        System.out.println("Tomorrow is a holiday");
     }
 }
