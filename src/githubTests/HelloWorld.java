15cd3063da50f8237d2b4aaa48f3fed13a2c29d1
TinaS-Test <silfvertina@gmail.com>
2025-12-23 10:46:28 +0100

    Now there is another message added

---
+++ src/githubTests/HelloWorld.java
@@ -0,0 +1,13 @@
+package githubTests;
+
+public class HelloWorld {
+    static void main(String[] args) {
+        System.out.println("Hello Tomas!");
+        System.out.println("Hello class!");
+        System.out.println("Hello world!");
+
+        String message = "Hello hello";
+        System.out.println("I will no write a message from a string!");
+        System.out.println(message);
+    }
+}
