8092cb65727b9cdd2627f73ffaf39e4d162c01d8
TinaS-Test <silfvertina@gmail.com>
2025-12-23 11:05:02 +0100

    Removed the first message, and added another

--- src/githubTests/HelloWorld.java
+++ src/githubTests/HelloWorld.java
@@ -2,10 +2,6 @@ package githubTests;
 
 public class HelloWorld {
     static void main() {
-        String message = "Hello hello";
-        System.out.println("I will no write a message from a string!");
-        System.out.println(message);
-
         String wishes = "Have a great day";
         System.out.println(wishes);
 
